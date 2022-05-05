c2ad93432dd987df97f368bc53365a8e7a33e7ed
Nazir Louis <nazirlouis38@gmail.com>
2022-05-04 20:19:48 -0400

    Basic Url and View Set Up

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def projects(request):
+    msg = 'Hello, you are on projects page'
+    return render(request, 'projects.html',{'msg':msg})
+
+def project(request, pk):
+    return render(request, 'single-project.html')
